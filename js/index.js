12e747e21ff039770900040fc263dd30512571a1
Matulaz <matiaszarate.psi@gmail.com>
2020-09-03 14:43:44 -0300

    agrego login

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function login() {
+    var usuario = document.getElementById("usuario").value;
+    var pass = document.getElementById("pass").value;
+
+    if (usuario == "admin" && pass == "admin"){
+        alert("Has iniciado sesion");
+        window.location = "index.html";
+    }
+    else{
+        throw new Error('¡Upsssssssss!')
+    }
+}
