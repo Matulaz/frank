5ad5fb4f9162c493336ce2cccad18ffb340286c7
Matulaz <matiaszarate.psi@gmail.com>
2020-09-08 14:44:47 -0300

    login actualizado

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function login() {
     var pass = document.getElementById("pass").value;
 
     if (usuario == "admin" && pass == "admin"){
-        alert("Has iniciado sesion");
+        //alert("Has iniciado sesion");
         window.location = "index.html";
     }
     else{
